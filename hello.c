c2dc9643b4b4e5ebd2177141cf327e1b6f972c68
unknown <seck.bakar@ugb.edu.sn>
2022-12-30 15:57:25 +0000

    done

--- hello.c
+++ hello.c
@@ -19,14 +19,8 @@ void PrintString(char str[]){
 int main(){
 
     char chaine[25] = "Hello World !";
-    int i = 0;
-
-    while (chaine[i] != '\0')
-    {
-        PrintChar(chaine[i]);
-        i++;
-    }
-
+   
+    PrintString(chaine);
     NextLine();
     
     return 0;
