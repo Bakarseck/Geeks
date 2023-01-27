3679b08801fa8bed4976f6e9751d1c952b969ba8
Bakarseck <86267786+Bakarseck@users.noreply.github.com>
2023-01-27 16:48:16 +0000

    Update sort.h

--- library/sort.h
+++ library/sort.h
@@ -2,18 +2,18 @@
 #define SORT_H
 #include "lib.h"
 
-void InsertSort(int t[], int n) {
+void InsertSort(int* t, int n) {
     int j = 0 ;
     int temp = 0;
-    while (j < n) {
+    while (j < n) { //tant j est inférieur à n
         for (int i = 1; i < n; i++ ) {
-            if (t[i-1] > t[i]) {
+            if (t[i-1] > t[i]) { // Si le tableau n'est pas trié
                 temp = t[i-1] ;
                 t[i-1] = t[i] ;
-                t[i] = temp ;
-                j = 0 ;
+                t[i] = temp ; // Je permute
+                j = 0 ; // Je reviens à la première ligne du tableau pour trier les premiers éléments
             } else {
-                j += 1 ;
+                j += 1 ; // si le tableau est trié, il ajoute 1 dans j
             }
         }
     }
